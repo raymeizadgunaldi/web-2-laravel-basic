4fdbc49a06bc1753a3adf76450976f1a5fc22c48
Raymeizad Gunaldi <raymeizadgnd@gmail.com>
2026-05-06 16:57:33 +0800

    Pertemuan 7: Searching, Pagination, Filter & Show

--- app/Http/Controllers/DepartmentController.php
+++ app/Http/Controllers/DepartmentController.php
@@ -39,7 +39,10 @@ class DepartmentController extends Controller
      */
     public function show(Department $department)
     {
-        //
+              return view('department.show', [
+        'title' => 'Detail Department ' . $department->name,
+        'department' => $department,
+        ]);
     }
 
     /** 
--- resources/views/lecturer/index.blade.php
+++ resources/views/lecturer/index.blade.php
@@ -10,10 +10,35 @@
 
 <a class="btn btn-primary mb-3" href="{{ route('lecturer.create') }}" role="button">Create</a>
 
+<form action="">
+
+<div class="row g-3 mb-3">
+    <div class="col-md-4">
+        <input type="text" class="form-control" id="keyword" name="keyword" placeholder="Search lecturer name ..." value="{{ request('keyword') }}">
+    </div>
+    <div class="col-md-4">
+        <select class="form-select" id="department_id" name="department_id">
+  <option value="">All Department</option>
+  @foreach ( $departments as $department )
+  <option value="{{ $department->id }}"
+     {{ request('department_id') == $department->id ? 'selected' : '' }}
+     >
+    {{ $department->name }}
+</option>
+  @endforeach
+</select>
+    </div>
+    <div class="col-md-4">
+          <button type="submit" class="btn btn-success">Search</button>
+    </div>
+</div>
+
+</form>
+
 <ul class="list-group">
    @foreach ($lecturers as $lecturer)
    <li class="list-group-item">
-    {{ $loop->iteration }}. {{ $lecturer->name }} -- {{ $lecturer->department->name }}
+    {{ $lecturers->firstItem() + $loop->index }}. {{ $lecturer->name }} -- {{ $lecturer->department->name }}
     <a class="btn btn-warning btn-sm" href="{{ route('lecturer.edit', $lecturer) }}" role="button">edit</a>
     <form action="{{ route('lecturer.destroy', $lecturer) }}" method="POST" class="d-inline">
     @method('DELETE')
@@ -30,6 +55,6 @@
       
 </ul>
 
-
+{{ $lecturers->links() }}
 
 </x-app>
